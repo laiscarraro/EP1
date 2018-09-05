e502f4146f087517f4867a2706f8760a16f5f49a
laiscarraro <37347778+laiscarraro@users.noreply.github.com>
2018-09-05 13:22:49 -0300

    Update EP1.c

--- EP1.c
+++ EP1.c
@@ -51,13 +51,14 @@ void exibir(NO* inicio) {
 }
 
 //Inverter a frase inteira, q seria o passo 2
-NO* passo2(NO* frase) {
+NO* passo2(NO* frase, NO* *ult) {
 	NO* inicio = NULL;
 	NO* p = frase;
 	while(p) {
 		NO* novo = (NO*) malloc(sizeof(NO));
 		novo->letra = p->letra;
 		novo->prox = inicio;
+		if(!inicio) *ult = novo;
 		inicio = novo;
 		p = p->prox;
 	}
@@ -66,60 +67,65 @@ NO* passo2(NO* frase) {
 
 NO* passo1(NO* vogal1prox, NO* vogal2, NO* *ultimo) {
 	NO* resp = NULL;
-	NO* ult = NULL;
 
 	if(vogal1prox == vogal2) return NULL;
 
 	while(vogal1prox != vogal2){
 		NO* novo = (NO*) malloc(sizeof(NO));
 		novo->letra = vogal1prox->letra;
-		novo->prox = NULL;
-		if(!resp) resp = novo;
-		else ult->prox = novo;
-		ult = novo;
+		novo->prox = resp;
+		if(!resp) *ultimo = novo;
+		resp = novo;
 		vogal1prox = vogal1prox->prox;
 	}
-	*ultimo = ult;
 	return resp;
 }
 
 int ehVogal(char a) {
 	if(a != 'a' && a != 'e' && a != 'i' && a != 'o' && a != 'u' && a != 'A' && a != 'E' && a != 'I' && a != 'O' && a != 'U'){
-		return true;
-	} else {
 		return false;
+	} else {
+		return true;
 	}
 }
 
 
 NO* codificar(NO* frase) {
 	NO* resp = NULL;
+	NO* final = NULL;
 
-	NO* vogal1 = frase;
-	NO* vogal2 = frase;
+	NO* sla = frase;
 
-	if(!ehVogal(vogal1->letra) && vogal1 == frase){
-		while(!ehVogal(vogal2->letra) && vogal2) vogal2 = vogal2->prox;
-		NO* ultimo = NULL;
-		resp = passo2(vogal1, vogal2, ultimo);
-		NO* novo = (NO*) malloc(sizeof(NO));
-		novo->letra = vogal2->letra;
-		novo->prox = NULL;
-		ultimo->prox = novo;
-
-	} else if(ehVogal(vogal1->letra) && vogal1 == frase){
-		vogal2 = vogal2->prox;
-		while(!ehVogal(vogal2->letra) && vogal2) vogal2 = vogal2->prox;
-		NO* ultimo = NULL;
-		NO* novo1 = (NO*) malloc(sizeof(NO));
-		novo1->letra = vogal1->letra;
-		novo1->prox = passo2(vogal1->prox, vogal2, ultimo);
-		if(!novo1->prox) ultimo = novo1;
-		resp = novo1;
-		NO* novo = (NO*) malloc(sizeof(NO));
-		novo->letra = vogal2->letra;
-		novo->prox = NULL;
-		ultimo->prox = novo;
+	while(sla) {
+		NO* vogal1 = sla;
+		NO* vogal2 = sla;
+		NO* inicio = final;
+		NO* fim = NULL;
+		if(!ehVogal(vogal1->letra)){
+			while(vogal2 && vogal2->prox && !ehVogal(vogal2->letra)) vogal2 = vogal2->prox;
+			NO* p = passo1(vogal1, vogal2, &fim);
+			if(inicio) inicio->prox = p;
+			inicio = p;
+		} else {
+			vogal2 = vogal2->prox;
+			while(vogal2 && vogal2->prox && !ehVogal(vogal2->letra)) vogal2 = vogal2->prox;
+			NO* novo1 = (NO*) malloc(sizeof(NO));
+			novo1->letra = vogal1->letra;
+			novo1->prox = passo1(vogal1->prox, vogal2, &fim);
+			if(!novo1->prox) fim = novo1;
+			inicio = novo1;
+		}
+		if(vogal2) {
+			NO* novo = (NO*) malloc(sizeof(NO));
+			novo->letra = vogal2->letra;
+			novo->prox = NULL;
+			fim->prox = novo;
+			fim = novo;
+		}
+		if(!resp) resp = inicio;
+		final = fim;
+		if(vogal2) sla = vogal2->prox;
+		else sla = vogal2;
 	}
 	return resp;
 }
@@ -127,15 +133,11 @@ NO* codificar(NO* frase) {
 int main() {
 	NO* p = NULL;
 	char frase[500];
-	strcpy(frase, "ESTRUTURA DE DADOS E MUITO LEGAL.");
+	strcpy(frase, "ESTRUTURA DE DADOS.");
 	fraseParaLista(&p, frase);
-
-	NO* teste = p;
-	NO* vogal1 = teste->prox->prox;
-	NO* vogal2 = teste->prox->prox->prox->prox->prox;
 
 	exibir(p);
 	printf("\n");
-	exibir(passo1(vogal1->prox, vogal2));
+	exibir(codificar(p));
 	printf("\n");
 }
