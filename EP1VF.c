fdf5c136bef8a001622cb8d857223e934875dac9
laiscarraro <37347778+laiscarraro@users.noreply.github.com>
2018-09-18 17:06:41 -0300

    Update EP1VF.c

--- EP1VF.c
+++ EP1VF.c
@@ -144,7 +144,7 @@ NO* codificar(NO* frase) {
 		    	} else {
 		    	    vogal1->prox = inverteTudo(vogal1->prox);
 		    	}
-		    	return frase;
+		    	return inverteTudo(frase);
 		    }
 		    
 			j = passo1Vogal(vogal1);
@@ -156,7 +156,7 @@ NO* codificar(NO* frase) {
 			andou = 0;
 		} 
 	} while(j); //executa j+1 vezes
-	return frase;
+	return inverteTudo(frase);
 }
 
 int main() {
@@ -164,23 +164,6 @@ int main() {
 	char frase[500];
 	setlocale(LC_ALL,"pt_BR.UTF-8");
 	strcpy(frase, "ESTRUTURAS DE DADOS E MUITO LEGAL.");
-	// strcpy(frase, "a12345ei");
-	// strcpy(frase, "a12e");
-	// strcpy(frase, "ae");
-	// strcpy(frase, "12345a");
-	// strcpy(frase, "123a");
-	// strcpy(frase, "12a");
-	// strcpy(frase, "123"); 
-	// strcpy(frase, "a123456789");
-	// strcpy(frase, "123456789");
-	// strcpy(frase, "a12");//nok
-	// strcpy(frase, "12");	//nok
-	// strcpy(frase, "a2");	//nok
-	// strcpy(frase, "1");
-	// strcpy(frase, "a");
-	// strcpy(frase, "5i12");
-    // strcpy(frase, "PUTA QUE ME PARIU!");
-
 
 	fraseParaLista(&p, frase);
 	printf("Frase original: ");
@@ -189,83 +172,4 @@ int main() {
     printf("Apos o passo 1: ");
 	exibir(codificar(p));
 	printf("\n");
-	printf("Apos o passo 2: ");
-	exibir(inverteTudo(codificar(p)));
-	printf("\n");
 }
-
-// -------------------------------------------Famoso lixo----------------------------------------------------
-
-/*int encontraVogal(){
-	if(!ehVogal(vogal1->letra)){
-		NO* p = passo1(vogal1, vogal2, &fim);
-		if(inicio) inicio->prox = p;
-		inicio = p;
-	}
-}*/
-/*//Inverter a frase inteira, q seria o passo 2
-NO* passo2(NO* frase, NO* *ult) {
-	NO* inicio = NULL;
-	NO* p = frase;
-	while(p) {
-		NO* novo = (NO*) malloc(sizeof(NO));
-		novo->letra = p->letra;
-		novo->prox = inicio;
-		if(!inicio) *ult = novo;
-		inicio = novo;
-		p = p->prox;
-	}
-	return inicio;
-}*/
-
-/*NO* passo1(NO* vogal1prox, NO* vogal2, NO* *ultimo) {
-	NO* resp = NULL;
-	if(vogal1prox == vogal2) return NULL;
-	while(vogal1prox != vogal2){
-		NO* novo = (NO*) malloc(sizeof(NO));
-		novo->letra = vogal1prox->letra;
-		novo->prox = resp;
-		if(!resp) *ultimo = novo;
-		resp = novo;
-		vogal1prox = vogal1prox->prox;
-	}
-	return resp;
-}*/
-/*if (j->prox->prox && ultima == 0)
-{
-	printf("pqqqqqq %s\n",j->prox->prox);
-	ultima=1;
-}
-o segundo passo 1
-	    a = n->letra;
-	    n->prox->prox = n;
-	    a = n->prox->letra;
-	    n->prox = t;
-	    a = t->letra;
-	    n = p;
-*/
-/*NO* passo1Consoante(NO* n) {
-    char a = n->letra;
-	printf("Iniciou o passo1V2 \n");
-	NO* x = n->prox;
-	NO* p = x->prox;
-	NO* t = p->prox;
-	while(t != n && t->prox){ //invertendo as parada tudo
-		p->prox = x;
-		x = p;
-		p = t;
-		t = t->prox;
-	}
-	    p->prox = x;
-    	n->prox->prox = t;
-	    n->prox = p;
-	return t;
-}*/
-		/*printf("aaaaaa\n");
-		if(t->prox){
-			t->prox = g;
-			p->prox = x;
-			x = p;
-			p = t;
-			break;
-		}*/
